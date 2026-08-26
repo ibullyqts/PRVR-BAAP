3fe55fa653262da89a2ef6a546a45dfe8a5b4a02
ibullyqts <sexenfonce@gmail.com>
2026-08-26 16:00:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ SIGNATURE = "༺ρ 𝕣 ꪜ 𝕣 अब्बू ☽༻"
 SIGNATURE_CHANCE = 0.15 
 
 def get_payload():
-    base_text = "KNOX-SURAJ ᴛʀʏ. ᴍᴀ ғʟᴏᴡᴇʀ."
+    base_text = "KRISH-OBLIVION-YUVI-GAARA ᴛʀʏ. ᴍᴀ ғʟᴏᴡᴇʀ."
     fire_part = "ʏᴀ ғɪʀᴇ 🔥??"
     flowers = ["🌸", "🌹", "🌺", "🌻", "🌼", "🌷"]
     line = f"{base_text} {random.choice(flowers)} {fire_part}"
